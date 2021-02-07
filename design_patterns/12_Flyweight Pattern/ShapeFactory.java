4d4a7dc9d84d180160c42f7af6c99f9ed0a64b44
miku-wwl <38804125+miku-wwl@users.noreply.github.com>
2021-02-07 13:51:35 +1100

    2021/02/07

--- design_patterns/12_Flyweight Pattern/ShapeFactory.java
+++ design_patterns/12_Flyweight Pattern/ShapeFactory.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 public class ShapeFactory {
     private static final HashMap<String,Shape> circleMap = new HashMap<>();
     public static Shape getCircle(String color){
-        Circle circle = (Circle)circleMap.get(color)
+        Circle circle = (Circle)circleMap.get(color);
         
         if (circle == null){
             circle = new Circle(color);
