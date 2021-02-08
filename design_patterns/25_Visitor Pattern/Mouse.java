ed86767379df8cc29658d16819bb5e7b086591de
miku-wwl <38804125+miku-wwl@users.noreply.github.com>
2021-02-08 13:14:31 +1100

    2021/02/08

---
+++ design_patterns/25_Visitor Pattern/Mouse.java
@@ -0,0 +1,10 @@
+public class Mouse implements ComputerPart{
+
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+        computerPartVisitor.visit(this);
+
+    }
+    
+    
+}
